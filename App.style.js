caf02fee42c2fd536a1b7a15460ca5b108dd06f1
DoomWave <acunanajair9@gmail.com>
2024-10-03 16:38:41 -0700

    using flex to see the layout.

--- App.style.js
+++ App.style.js
@@ -1,5 +1,20 @@
 import { StyleSheet } from "react-native";
 
 export const s = StyleSheet.create({
-    
+    app: {
+        flex: 1,
+        backgroundColor: "#F9F9F9",
+    },
+    header: {
+        flex: 1,
+        backgroundColor: "red"
+    },
+    body: {
+        flex: 5,
+        backgroundColor: "blue"
+    },
+    footer: {
+        flex: 1,
+        backgroundColor: "green"
+    },
 });
