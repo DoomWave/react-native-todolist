07fd6e5cad457175990f5604dea64257c2eba57a
DoomWave <acunanajair9@gmail.com>
2024-10-03 17:32:15 -0700

    working in the card component.

--- components/CardTodo/CardTodo.jsx
+++ components/CardTodo/CardTodo.jsx
@@ -1,4 +1,9 @@
-import { s } from "./CardTodo.style"
-export function CardTodo() {
-    return <></>;
+import { Image, Text, TouchableOpacity } from "react-native";
+import { s } from "./CardTodo.style";
+import checkImg from "../../assets/check";
+export function CardTodo({todo}) {
+    return <TouchableOpacity>
+        <Text>{todo.title}</Text>
+        <Image source={checkImg}/>
+    </TouchableOpacity>;
 }
