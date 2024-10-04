4ff9883d1ae0c24d5f9a28a3ac807959179dc6a8
DoomWave <acunanajair9@gmail.com>
2024-10-04 16:17:56 -0700

    IDK how to fix this problem of adding more todos.

--- App.js
+++ App.js
@@ -3,38 +3,40 @@ import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context"
 import { s } from "./App.style";
 import { Header } from "./components/Header/Header";
 import { CardTodo } from "./components/CardTodo/CardTodo";
-import {useState } from "react";
+import {useState, useEffect } from "react";
 import { TabBottomMenu } from "./components/TabBottomMenu/TabBottomMenu";
 import { ButtonAdd } from "./components/ButtonAdd/ButtonAdd";
+import Dialog from "react-native-dialog"
+import uuid from 'react-native-uuid';
 
 export default function App() {
   const [todoList, setTodoList] = useState([]);
-  const [selectedTabName, setSelectedTabName] = useState("all")
+  const [selectedTabName, setSelectedTabName] = useState("all");
+  const [isAddDialogDisplayed, setIsAddDialogDisplayed] = useState(false);
+  const [inputValue, setInputValue] = useState("");
 
-function getFilteredList(){
-    switch(selectedTabName){
+  function getFilteredList() {
+    switch (selectedTabName) {
       case "all":
-        return todoList
+        return todoList;
       case "inProgress":
-        return todoList.filter((todo)=> !todo.isCompleted);
+        return todoList.filter((todo) => !todo.isCompleted);
       case "done":
-        return todoList.filter((todo)=> todo.isCompleted);
+        return todoList.filter((todo) => todo.isCompleted);
     }
   }
 
-  function deleteTodo(todoToDelete){
-    Alert.alert("Delete todo", "Are you sure you want to delete this todo?",
-      [
+  function deleteTodo(todoToDelete) {
+    Alert.alert("Delete todo", "Are you sure you want to delete this todo ?", [
       {
         text: "Delete",
-        style:"destructive",
-        onPress:() => {
-          setTodoList(todoList.filter(t => t.id !== todoToDelete.id))
+        style: "destructive",
+        onPress: () => {
+          setTodoList(todoList.filter((t) => t.id !== todoToDelete.id));
         },
       },
-      {text: "Cancel", style:"cancel"},
-      ]
-    );
+      { text: "Cancel", style: "cancel" },
+    ]);
   }
 
   function renderTodoList() {
@@ -57,26 +59,65 @@ function getFilteredList(){
     updatedTodoList[indexToUpdate] = updatedTodo;
     setTodoList(updatedTodoList);
   }
+
+  function addTodo() {
+    const newTodo = {
+      id: uuid.v4(),
+      title: inputValue,
+      isCompleted: false,
+    };
+    setTodoList([...todoList, newTodo]);
+    setIsAddDialogDisplayed(false);
+    setInputValue("");
+  }
+
+  function renderAddDialog() {
+    return (
+      <Dialog.Container
+        visible={isAddDialogDisplayed}
+        onBackdropPress={() => setIsAddDialogDisplayed(false)}
+      >
+        <Dialog.Title>Add todo</Dialog.Title>
+        <Dialog.Description>Choose a name for your todo</Dialog.Description>
+        <Dialog.Input
+          onChangeText={setInputValue}
+          placeholder="Ex : Go to the dentist"
+        />
+        <Dialog.Button
+          label="Cancel"
+          color="grey"
+          onPress={() => setIsAddDialogDisplayed(false)}
+        />
+        <Dialog.Button
+          disabled={inputValue.length === 0}
+          label="Save"
+          onPress={addTodo}
+        />
+      </Dialog.Container>
+    );
+  }
+
   return (
     <>
-    <SafeAreaProvider>
-      <SafeAreaView style={s.app}>
-        <View style={s.header}>
-          <Header />
-        </View>
-        <View style={s.body}>
-          <ScrollView>{renderTodoList()}</ScrollView>
-        </View>
-        <ButtonAdd />
-      </SafeAreaView>
-    </SafeAreaProvider>
-    <View style={s.footer}>
-      <TabBottomMenu
-        todoList={todoList}
-        onPress={setSelectedTabName}
-        selectedTabName={selectedTabName}
-      />
-    </View>
-  </>
+      <SafeAreaProvider>
+        <SafeAreaView style={s.app}>
+          <View style={s.header}>
+            <Header />
+          </View>
+          <View style={s.body}>
+            <ScrollView>{renderTodoList()}</ScrollView>
+          </View>
+          <ButtonAdd onPress={() => setIsAddDialogDisplayed(true)} />
+        </SafeAreaView>
+      </SafeAreaProvider>
+      <View style={s.footer}>
+        <TabBottomMenu
+          todoList={todoList}
+          onPress={setSelectedTabName}
+          selectedTabName={selectedTabName}
+        />
+      </View>
+      {renderAddDialog()}
+    </>
   );
 }
--- components/ButtonAdd/ButtonAdd.jsx
+++ components/ButtonAdd/ButtonAdd.jsx
@@ -1,8 +1,9 @@
 import { Text, TouchableOpacity } from "react-native";
 import { s } from "./ButtonAd.style";
-
-export function ButtonAdd(){
-    return <TouchableOpacity style={s.btn}>
-        <Text style={s.txt}>+ New todo</Text>
+export function ButtonAdd({OnPress}){
+    return (
+    <TouchableOpacity OnPress={OnPress} style={s.btn}>
+      <Text style={s.txt}>+ New todo</Text>
     </TouchableOpacity>
+    );
 }
